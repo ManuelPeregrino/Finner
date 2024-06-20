e8dc9691ecf76167f163ef31aac4c7da598dbc2e
Diego Carmona Bernal <cbdiegox@gmail.com>
2024-06-20 13:20:53 -0600

    Login

---
+++ lib/views/home_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Map<String, String>> reminders = [
+    {
+      'task': 'FIG-121',
+      'title': 'Write blog post for demo day',
+      'priority': 'Alto'
+    },
+    {'task': 'FIG-122', 'title': 'Publish blog page', 'priority': 'Bajo'},
+    {
+      'task': 'FIG-123',
+      'title': 'Add gradients to design sys...',
+      'priority': 'Medio'
+    },
+    {
+      'task': 'FIG-124',
+      'title': 'Responsive behavior doesn\'t...',
+      'priority': 'Medio'
+    },
+    {
+      'task': 'FIG-125',
+      'title': 'Confirmation states not ren...',
+      'priority': 'Medio'
+    },
+    {
+      'task': 'FIG-126',
+      'title': 'Revise copy on the About p...',
+      'priority': 'Bajo'
+    },
+    {
+      'task': 'FIG-127',
+      'title': 'Text wrapping is awkward o...',
+      'priority': 'Bajo'
+    },
+    {'task': 'FIG-128', 'title': 'Publish HackerNews post', 'priority': 'Bajo'},
+    {
+      'task': 'FIG-129',
+      'title': 'Review image licensing for...',
+      'priority': 'Alto'
+    },
+    {
+      'task': 'FIG-130',
+      'title': 'Accessibility focused state f...',
+      'priority': 'Alto'
+    },
+    {
+      'task': 'FIG-131',
+      'title': 'Header IA revision to suppo...',
+      'priority': 'Bajo'
+    },
+  ];
+
+  Widget _buildReminderItem(Map<String, String> reminder) {
+    Color priorityColor;
+    switch (reminder['priority']) {
+      case 'Alto':
+        priorityColor = Colors.red;
+        break;
+      case 'Medio':
+        priorityColor = Colors.orange;
+        break;
+      case 'Bajo':
+      default:
+        priorityColor = Colors.green;
+        break;
+    }
+
+    return ListTile(
+      leading: Text(reminder['task']!),
+      title: Text(reminder['title']!),
+      trailing: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        decoration: BoxDecoration(
+          color: priorityColor,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Text(
+          reminder['priority']!,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.search, color: Colors.black),
+                      labelText: 'Buscar',
+                      labelStyle: const TextStyle(color: Colors.black),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                SizedBox(
+                  height: 48, // Altura del TextField
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black, // Color del icono
+                      minimumSize:
+                          const Size(55, 55), // Tamaño mínimo del botón
+                      padding: EdgeInsets.zero, // Sin relleno interno
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            4), // Ajuste del radio de las esquinas
+                      ),
+                    ),
+                    child: const Icon(Icons.filter_alt_outlined),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Tarea', style: TextStyle(fontWeight: FontWeight.bold)),
+                Text('Titulo', style: TextStyle(fontWeight: FontWeight.bold)),
+                Text('Prioridad',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+          const Divider(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: reminders.length,
+              itemBuilder: (context, index) {
+                return _buildReminderItem(reminders[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/login_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'FINNER',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              'Crear una cuenta',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'Ingrese su correo electrónico para registrarse en esta aplicación',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 32),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'correo@dominio.com',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text('Regístrese con correo electrónico',
+                  style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: const [
+                Expanded(child: Divider(color: Colors.grey)),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text('o continuar con',
+                      style: TextStyle(color: Colors.grey)),
+                ),
+                Expanded(child: Divider(color: Colors.grey)),
+              ],
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                side: const BorderSide(color: Colors.grey),
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              icon:
+                  const Icon(Icons.g_mobiledata_outlined, color: Colors.black),
+              label:
+                  const Text('Google', style: TextStyle(color: Colors.black)),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              'Al hacer clic en continuar, acepta nuestros Términos de servicio y Política de privacidad',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
